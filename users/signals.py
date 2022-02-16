bc2eef09454b63bbdf38375c9e851b955f693539
Bill Williams <divebilldive@gmail.com>
2022-02-15 17:20:50 -0800

    New users have a domain and area created for them. Via signals in the user app.

--- users/signals.py
+++ users/signals.py
@@ -2,15 +2,17 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from wi.models import domain, area
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-   # instance.profile.save()
-   pass
+        #
+        # each user should have a single default domain and area
+        #
+        default_domain = domain(name='Personal', created_by=instance)
+        default_domain.save()
+        default_area = area(name='Home', domain=default_domain, created_by=instance)
+        default_area.save()
