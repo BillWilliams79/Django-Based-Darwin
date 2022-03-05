4f30195e1baa829ced56b8a6533e18f5c96371ff
Bill Williams <divebilldive@gmail.com>
2022-03-05 01:16:44 -0800

    trial code requesting full screen

--- static/javascript/task_base.js
+++ static/javascript/task_base.js
@@ -7,6 +7,9 @@ $(document).ready(
         //
         check_set_grid_sizes();
 
+        console.log('tfs');
+        toggleFullScreen();
+
         //
         // register: app grid setup on resize
         //
@@ -88,4 +91,19 @@ function check_set_grid_sizes() {
     }
 }
 
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+  
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+  
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+      requestFullScreen.call(docEl);
+      console.log('requesting fs');
+    }
+    else {
+      cancelFullScreen.call(doc);
+    }
+  }
 
