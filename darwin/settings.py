60d896ee61b8a2f008ffc55e02907d6e3cbc8f64
Bill Williams <divebilldive@gmail.com>
2022-03-16 14:18:33 -0700

    Strange to set this in settings, but it enables pagination with base classes I'm using

--- darwin/settings.py
+++ darwin/settings.py
@@ -206,3 +206,11 @@ LOGGING = {
 }
 
 django_heroku.settings(locals())
+
+#
+# Django Rest Framework
+#
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
